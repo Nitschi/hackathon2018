3cd5e912a1a304463a887eb2789ca8c7a6b3a6d1
Johannes <ufdoi@student.kit.edu>
2018-03-24 17:09:23 +0100

    work on the serial connection
    Signed-off-by: Johannes <ufdoi@student.kit.edu>

--- app/src/main/java/com/beertastic/beertastic/MainActivity.java
+++ app/src/main/java/com/beertastic/beertastic/MainActivity.java
@@ -1,34 +1,85 @@
 package com.beertastic.beertastic;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    TextView t1;
+    ScaleConnector scale;
+    UsbManager usbManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        b1 = (Button)findViewById(R.id.buttonGetLastSerialOutput);
+        t1 = (TextView)findViewById(R.id.textViewSerialOuput);
+
+        b1.setOnClickListener(b1OnClick);
+
+        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
+        scale = new ScaleConnector(this, usbManager);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ScaleConnector.ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
+
     }
 
-    @Override
+    OnClickListener b1OnClick = new OnClickListener(){
+        public void onClick(View v)
+        {
+            t1.setText("new int: " + scale.getLastReceivedMessage());
+        }
+
+
+
+    };
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ScaleConnector.ACTION_USB_PERMISSION)) {
+                boolean granted =
+                        intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    scale.connectToScale();
+                    //b1.setEnabled(true);
+                } else {
+                    Log.d("SERIAL", "PERM NOT GRANTED");
+                }
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+                scale.onUsbConnect();
+                //b1.setEnabled(true);
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                scale.onUsbDisconnect();
+                //b1.setEnabled(false);
+            }
+        };
+    };
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
---
+++ app/src/main/java/com/beertastic/beertastic/ScaleConnector.java
@@ -0,0 +1,119 @@
+package com.beertastic.beertastic;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Johannes on 24.03.2018.
+ */
+
+public class ScaleConnector {
+    public static final String ACTION_USB_PERMISSION = "com.hariharan.arduinousb.USB_PERMISSION";
+    UsbDevice device;
+    UsbDeviceConnection connection;
+    UsbSerialDevice serialPort;
+    android.content.Context context;
+    UsbManager usbManager;
+
+    private String lastReceivedMessage = "";
+
+    public  ScaleConnector(android.content.Context context, UsbManager usbManager)
+    {
+        this.context = context;
+        this.usbManager = usbManager;
+    }
+
+    public void connectToScale()
+    {
+        Log.i("serial", "connectToScale called");
+        connection = usbManager.openDevice(device);
+        serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+        if (serialPort != null) {
+            if (serialPort.open()) { //Set Serial Connection Parameters.
+                serialPort.setBaudRate(9600);
+                serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                serialPort.read(mScaleMeasurementReceivedCallback); //
+                Log.i("SERIAL", "serial connection estabilshed");
+
+            } else {
+                Log.d("SERIAL", "PORT NOT OPEN");
+            }
+        } else {
+            Log.d("SERIAL", "PORT IS NULL");
+        }
+    }
+    byte[] currentBytes;
+
+    private UsbSerialInterface.UsbReadCallback mScaleMeasurementReceivedCallback = new UsbSerialInterface.UsbReadCallback() {
+
+        @Override
+        public void onReceivedData(byte[] arg0)
+        {
+            byte[] carryFeed = new byte[] {13, 10};
+
+            Log.i("serial", "byte array length: " + arg0.length);
+
+            if ((arg0.length == 2) && (arg0[0] == carryFeed[0]) && (arg0[1] == carryFeed [1]))
+            {
+                Log.i("serial", "new line");
+                return;
+            }
+
+            lastReceivedMessage = new String(arg0);;
+            Log.i("serial", lastReceivedMessage);
+        }
+
+    };
+
+    public String getLastReceivedMessage() {
+        return lastReceivedMessage;
+    }
+
+    public void onUsbConnect()
+    {
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                int deviceVID = device.getVendorId();
+                if (deviceVID == 0x2341)//Arduino Vendor ID
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep)
+                    break;
+            }
+        }
+    }
+
+    public  void onUsbDisconnect()
+    {
+        try {
+            serialPort.close();
+        }
+        catch (Exception exception) {
+            Log.d("serial", "tried to close serial connection but serial connection was null " + exception.toString());
+        }
+    }
+}
