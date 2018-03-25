91363b0e652ba333c5ac6de1e6053a1d6f85991d
alexander <alexander.nitsch@posteo.de>
2018-03-25 05:55:30 +0200

    Alcohol Test as Activity

--- app/src/main/java/com/beertastic/beertastic/AlcoholTestActivity.java
+++ app/src/main/java/com/beertastic/beertastic/AlcoholTestActivity.java
@@ -1,13 +1,30 @@
 package com.beertastic.beertastic;
 
-
-/**
- * Created by alexander on 25.03.18.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AlcoholTestActivity extends ListenerRegisterActivity {
 
-    private double bloodAlcoholMax = 0;
+    private double maxBloodAlcohol = 0;
+    TextView promilleDisp;
+    Button okayButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alcohol_test);
+        promilleDisp = (TextView)findViewById(R.id.promilleDisplay);
+        okayButton = (Button)findViewById(R.id.promilleButton);
+        okayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
     @Override
     public void onWeightUpdate(double newWeight) {
 
@@ -15,10 +32,17 @@ public class AlcoholTestActivity extends ListenerRegisterActivity {
 
     @Override
     public void onAlcoholUpdate(double newBloodAlcohol) {
-        if(newBloodAlcohol > bloodAlcoholMax)
-        {
-            bloodAlcoholMax = newBloodAlcohol;
+
+        if(newBloodAlcohol > maxBloodAlcohol) {
+            maxBloodAlcohol = newBloodAlcohol;
         }
+        final double finalBloodAlcohol = maxBloodAlcohol;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                promilleDisp.setText(String.valueOf(finalBloodAlcohol) + "Promille");
+            }
+        });
     }
 
     @Override
