36bcc10332b861a4a9e988156151f764e71fdd4d
Johannes <ufdoi@student.kit.edu>
2018-03-25 01:02:29 +0100

    changed scale pins to digital pins 6 and 7

--- Arduino/HX711SerialBegin/HX711SerialBegin.ino
+++ Arduino/HX711SerialBegin/HX711SerialBegin.ino
@@ -10,7 +10,8 @@ void setup() {
   // parameter "gain" is ommited; the default value 128 is used by the library
   // HX711.DOUT	- pin #A1
   // HX711.PD_SCK	- pin #A0
-  scale.begin(A1, A0);
+  //scale.begin(A1, A0);
+  scale.begin(7, 6);
 
   /*
   Serial.println("Before setting up the scale:");
